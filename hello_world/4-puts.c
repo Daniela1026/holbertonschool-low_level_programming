a1514a7390dbe52ba0c97d888b4168873d2feca9
Daniela1026 <cartagenad49@gmail.com>
2022-06-13 21:28:33 -0700

    My first commit

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -5,12 +5,13 @@
 #include <stdio.h>
 
 /**
-* main - Prints "Programming is like building a 
+*main - Prints "Programming is like building a 
 *multilingual puzzle, followed by a new line.
 *
-* Return: Always 0.
+*Return: Always 0.
 */
-int main(void){
+int main(void)
+{
 puts("\"Programming is like building a multilingual puzzle"); 
 return (0);
 }
