dea28e92b91a7dedfd429973349410925e9dce3e
Daniela1026 <cartagenad49@gmail.com>
2022-06-14 14:45:54 -0700

    My first commit

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -1,8 +1,10 @@
-/**This program will assign a random number                                                                                                            
+/**
+*This program will assign a random number                                                                                                            
 *to the variable n each time it is executed
 *if the number is greater than 0: is positive
 *if the number is 0: is zero
-*if the number is less than 0: is negative*/
+*if the number is less than 0: is negative
+*/
 
 #include <stdlib.h>
 #include <time.h>
