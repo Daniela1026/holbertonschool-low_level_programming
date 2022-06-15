af104d401416d37d7ed29add4d9b354d3800da12
Daniela1026 <cartagenad49@gmail.com>
2022-06-14 21:24:06 -0700

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+* main - alphabet
+*
+* Return: 0
+*/
+/*putchar*/
+
+int main(void)
+		{
+		char alf;
+
+		for (alf = 'a'; alf <= 'z'; alf++)
+		{
+			putchar(alf);
+			printf("\n");
+		}
+		return (0);
+		} 
