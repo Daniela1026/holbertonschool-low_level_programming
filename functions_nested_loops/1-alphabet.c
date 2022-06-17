424eb438c090fd186f868eaf1a6b2e06c527594d
Daniela1026 <cartagenad49@gmail.com>
2022-06-16 19:32:20 -0700

    alphabet _putchar

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* main - alphabet lowercase
+*
+* print_alphabet _putchar
+*
+* Return: 0
+*/
+void print_alphabet(void)
+{
+	char alf = 'a';
+	
+	while (alf <= 'z')
+	{
+		_putchar(alf);
+		alf++;
+	}
+	_putchar('\n');
+}
