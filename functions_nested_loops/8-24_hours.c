484831ac023969a1d84c0ec5e12e95d6bc95a041
Daniela1026 <cartagenad49@gmail.com>
2022-06-17 21:14:09 -0700

    jack_bauer hour min

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
-* print_last_digit - number
-* @n: character value of an integer
+* jack_bauer - hour min
 *
 * Return: las_digit
 */
@@ -10,18 +9,16 @@ void jack_bauer(void)
 	int hour = 0;
 	int min = 0;
 
-	while (hour <= 23)
+	for (hour = 0; hour < 24; hour++)
 	{
-		hour++;
-
-		for (min = 0; min <=59; min++)
+		for (min = 0; min <= 59; min++)
 		{
-			_putchar(hour / 10 + '0');
-			_putchar(hour % 10 + '0');
+			_putchar((hour / 10) + '0');
+			_putchar((hour % 10) + '0');
 			_putchar(':');
-			_putchar(min / 10 + '0');
-			_putchar(min % 10 + '0');
+			_putchar((min / 10) + '0');
+			_putchar((min % 10) + '0');
 			_putchar('\n');
 		}
-	}	
+	}
 }
