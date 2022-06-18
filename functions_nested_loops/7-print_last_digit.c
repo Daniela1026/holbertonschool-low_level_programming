07f2d7134ac3a43fa5a618056fe39cd86f1bca86
Daniela1026 <cartagenad49@gmail.com>
2022-06-17 19:07:17 -0700

    las digit number

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -1,16 +1,17 @@
 #include "main.h"
 /**
-* _abs - value of an integer
+* print_last_digit - number
 * @n: character value of an integer
 *
-* Return: 1 : 0
+* Return: las_digit
 */
 int print_last_digit(int n)
 {
 	int last_digit = n % 10;
 
 	if (last_digit < 0)
-	       	last_digit *= -1;
+		
+		last_digit *= -1;
 
 	_putchar(last_digit + '0');
 
