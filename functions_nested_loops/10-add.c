2ece55689e8f1ee3211bf7beb92a4d82b3c30f1a
Daniela1026 <cartagenad49@gmail.com>
2022-06-17 23:47:37 -0700

    sum

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -1,9 +1,10 @@
 #include "main.h"
 /**
-* add - add sum one + two
-*
+* add - integers and return num one two
 * @n1: number one
 * @n2: number two
+*
+* Return: value one two
 */
 int add(int n1, int n2)
 {
