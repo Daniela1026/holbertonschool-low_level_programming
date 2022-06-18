c7c3b36e67a3a1c43ba9419ee2f67825eaaa8813
Daniela1026 <cartagenad49@gmail.com>
2022-06-17 22:41:50 -0700

    mult 0-9

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,35 @@
+#include "main.h"
+/**
+* times_table - multip
+*
+*/
+void times_table(void)
+{
+	int n;
+	int mult;
+	int resp;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar('0');
+
+		for (mult = 1; mult <= 9; mult++)
+		{
+			resp = n * mult;
+			_putchar(',');
+			_putchar(' ');
+
+			if (mult <= 0)
+			{
+			_putchar(' ');
+			}
+				else
+				{
+
+		_putchar(resp / 10 + '0');
+		_putchar(resp % 10 + '0');
+				}
+		}
+	_putchar('\n');
+	}
+}
