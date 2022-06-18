9479a496d835cbf3285a444527a770403cbf3206
Daniela1026 <cartagenad49@gmail.com>
2022-06-17 23:30:16 -0700

    sum

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,9 @@
+#include "main.h"
+/**
+* add - sum
+*
+*/
+int add(int n1, int n2)
+{
+	return (n1 + n2);
+}
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -5,31 +5,31 @@
 */
 void times_table(void)
 {
-	int n;
-	int mult;
-	int resp;
+        int n;
+        int mult;
+        int resp;
 
-	for (n = 0; n <= 9; n++)
-	{
-		_putchar('0');
+        for (n = 0; n <= 9; n++)
+        {
+               _putchar('0');
 
-		for (mult = 1; mult <= 9; mult++)
-		{
-			resp = n * mult;
+                for (mult = 1; mult <= 9; mult++)
+                {
+                        resp = n * mult;
 			_putchar(',');
-			_putchar(' ');
+                        _putchar(' ');
 
-			if (mult <= 0)
-			{
-			_putchar(' ');
-			}
-				else
-				{
+                        if (mult <= 0)
+                        {
+                        _putchar(' ');
+                        }
+                                else
+                                {
+                                        _putchar(resp / 10 + '0');
 
-		_putchar(resp / 10 + '0');
-		_putchar(resp % 10 + '0');
-				}
-		}
-	_putchar('\n');
-	}
+                                        _putchar(resp % 10 + '0');
+                                }
+                }
+        _putchar('\n');
+        }
 }
