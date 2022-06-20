b5748c3f6b8bea2c715466a7da217b6079ea6ed1
Daniela1026 <cartagenad49@gmail.com>
2022-06-20 15:43:32 -0700

    uppercase and otherwise character

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* _islower - if c is a letter
+* @c: Character int
+*
+* Return: 0
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'a')
+		return (1);
+			else
+				return (0);
+}
