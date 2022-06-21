fd6da531498416c61dbb00f039b89663a3bb3b75
Daniela1026 <cartagenad49@gmail.com>
2022-06-20 17:01:57 -0700

    numbers print

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* print_numbers - prints number
+*
+* Return: 0
+*/
+void print_numbers(void)
+{
+	char n = '0';
+
+	while (n <= '9')
+	{
+		_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
