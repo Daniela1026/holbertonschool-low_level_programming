73d3b83efc703cf40ab058597f0b371813cc3c90
Daniela1026 <cartagenad49@gmail.com>
2022-06-20 21:38:22 -0700

    character #

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -1,19 +1,24 @@
 #include "main.h"
 /**
-* print_diagonal - print \
-* @n: character int
+* print_square - print #
+* @size: character int
 * Return: 0
 */
 void print_square(int size)
 {
-        int l, lin;
+        int l = 0;
+	int lin = 0;
 
-        for (l = 0; l <= size; l++)
-        {
-                for (lin = 0; lin < size; lin++)
-                _putchar('#');
-                _putchar('\n');
-        }
         if (size < 1)
-        _putchar('\n');
+		_putchar('\n');
+	while (lin < size)
+	{
+		while (l < size)
+		{
+			_putchar('#');
+			l++;
+		}
+		_putchar('\n');
+		lin++;
+	}
 }
