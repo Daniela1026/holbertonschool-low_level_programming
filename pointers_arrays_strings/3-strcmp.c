18d259c25d4728b050de8e6d6e485e3163a5fa14
Daniela1026 <cartagenad49@gmail.com>
2022-06-29 17:09:17 -0700

    compares two strings

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,10 @@
 */
 int _strcmp(char *s1, char *s2)
 {
-        int c = 0;
+	int i = 0;
 
-        while (s1[c] != '\0' || s2[c] != '\0')
-                c++;
-        if (s1[c] != s2[c])
-	{
-		return (s1[c] - s2[c]);
-	}
-	return (0);
+	while (s1[i] != '\0' && s2[i] != '\0' && s1[i] == s2[i])
+		i++;
+	
+	return (s1[i] - s2[i]);
 }
