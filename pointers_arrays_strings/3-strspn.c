144ee38b975f4597efd4c3406321ced2827e61a6
Daniela1026 <cartagenad49@gmail.com>
2022-06-30 13:52:39 -0700

    prefix substring

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* _strchr - locates a character in a string
+* @s: Character char
+* @c: Character char
+*
+* Return: 1 and 0
+*/
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0;
+	int n;
+
+	while (s[i] != '\0')
+	{
+		for (n = 0; accept[n] != '\0' && s[i] != accept[n];n++)
+
+			if (accept[n] == '\0')
+				return (i);
+		i++;
+	}
+	return (i);
+}
