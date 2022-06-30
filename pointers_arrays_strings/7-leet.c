e8f415c9c19d9fa5bedb90eb81b5caa8ff164b0b
Daniela1026 <cartagenad49@gmail.com>
2022-06-29 21:01:41 -0700

    encodes

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,19 +8,15 @@
 char *leet(char *l)
 {
 	int i = 0;
+	int n;
+	char let[] = "aAeEoOtTlL";
+	char num[] = "43071";
 
 	for (i = 0; l[i] != '\0'; i++)
 	{
-		while (l[i] == 'a' || l[i] == 'A')
-			l[i] = '4';
-		while (l[i] == 'e' || l[i] == 'E')
-			l[i] = '3';
-		while (l[i] == 'o' || l[i] == 'O')
-			l[i] = '0';
-		while (l[i] == 't' || l[i] == 'T')
-			l[i] = '7';
-		while (l[i] == 'l' || l[i] == 'L')
-			l[i] = '1';
+		for (n = 0; let[n] != '\0'; n++)
+			if (l[i] == let[n])
+				l[i] = num[n / 2];
 	}
 	return (l);
 }
