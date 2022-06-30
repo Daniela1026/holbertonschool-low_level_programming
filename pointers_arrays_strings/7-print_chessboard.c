e5f1a350ea6f785a06183b55e19776df33a369cd
Daniela1026 <cartagenad49@gmail.com>
2022-06-30 16:41:15 -0700

    print pointer

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* print_chessboard - print chessboard
+* @a: Character char pointer
+*
+*/
+void print_chessboard(char (*a)[8])
+{
+	int i = 0;
+	int n;
+
+	while (i < 8)
+	{
+		for (n = 0; n < 8; n++)
+		{
+			_putchar(a[i][n]);
+		}
+		_putchar('\0');
+		i++;
+	}
+}
