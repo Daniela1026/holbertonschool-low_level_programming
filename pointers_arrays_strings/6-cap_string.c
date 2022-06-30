471414d920ad588e695735e70df6a796011aad42
Daniela1026 <cartagenad49@gmail.com>
2022-06-29 20:31:41 -0700

    encodes

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* string_toupper - lowercase and uppercase
+* @l: Character char
+*
+* Return: dest
+*/
+char *cap_string(char *)
+{
+}
---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+* leet - encodes into
+* @l: Character char
+*
+* Return: dest
+*/
+char *leet(char *l)
+{
+	int i = 0;
+
+	while (l[i] != '\0')
+	{
+		while (l[i] == 'a' || l[i] == 'A')
+			l[i] = '4';
+		while (l[i] == 'e' || l[i] == 'E')
+			l[i] = '3';
+		while (l[i] == 'o' || l[i] == 'O')
+			l[i] = '0';
+		while (l[i] == 't' || l[i] == 'T')
+			l[i] = '7';
+		while (l[i] == 'l' || l[i] == 'L')
+			l[i] = '1';
+		i++;
+	}
+	return (l);
+}
