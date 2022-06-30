a8c3a8cc06f60b59dd83be43f40387a1bdbdbe31
Daniela1026 <cartagenad49@gmail.com>
2022-06-30 11:45:02 -0700

    character string

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _memcpy - copies memory area
+* @dest: Character char
+* @src: Character char
+* @n: Character n
+*
+* Return: 1 and 0
+*/
+char *_strchr(char *s, char c)
+{
+	unsigned int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		if (*s == c)
+			return (s);
+		else if (*(s + i) == c)
+			return (s + i);
+	s++;
+	return (s + i);
+}
