d6b8dc88e3cc0213df020d484f0b58b082a35a17
Daniela1026 <cartagenad49@gmail.com>
2022-06-29 21:22:21 -0700

    space, tabulation, new line

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -1,10 +1,24 @@
 #include "main.h"
 /**
-* string_toupper - lowercase and uppercase
-* @l: Character char
+* cap_string - space, tabulation, new line
+* @n: Character char
 *
-* Return: dest
+* Return: n
 */
-char *cap_string(char *)
+char *cap_string(char *n)
 {
+	int i = 1;
+	int l;
+	char str[] = {',', ';', '.', '?', '"','(', ')', '{', '}', ' ', '\n', '\t'};
+
+	if (n[0] >= 'a' && n[0] <= 'z')
+		n[0] -= ('a' - 'A');
+	while (n[i] != '\0')
+	{
+		for (l = 0; str[l] != '\0'; l++)
+			if (n[i - 1] == str[l] && (n[i] >= 'a' && n[i] <= 'z'))
+				n[i] -= ('a' - 'A');
+		i++;
+	}
+	return (n);
 }
