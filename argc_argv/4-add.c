f3198c43714590a30d5e49c8eaaa037a59fc1364
Daniela1026 <cartagenad49@gmail.com>
2022-07-07 14:30:11 -0700

    adds positive numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+* main - prints program
+* @argc: Character int
+* @argv: Character char
+* Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	int i, x = 0;
+
+	if (argc < 1)
+		return (0);
+	
+	for (i = 1; i < argc; i++)
+	{
+		if (!atoi(argv[i]))
+		{
+			printf("%s\n", "Error");
+			return (1);
+		}
+		x += atoi(argv[i]);
+	}
+	printf("%d\n", x);
+	return (0);
+}
+
