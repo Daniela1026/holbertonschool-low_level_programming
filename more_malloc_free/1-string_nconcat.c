4aa3ad67bd1806ffede97d3472d9b8f2db7d5da7
Daniela1026 <cartagenad49@gmail.com>
2022-07-11 11:14:34 -0700

    concatenates two strings

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -16,10 +16,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL || s2 == NULL)
 		s1 = s2 = "";
 	
-	for (i = 0; s1[i]; i++)
+	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
-	for (s = 0; s2[s]; s++)
+	for (s = 0; s2[s] != '\0'; s++)
 	{
 	}
 	if (n < s)
