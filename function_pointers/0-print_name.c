5c0f33c6576a17d75bef1e20dda984dfc2bad0d2
Daniela1026 <cartagenad49@gmail.com>
2022-07-14 09:43:33 -0700

    That prints a name

---
+++ function_pointers/0-print_name.c
@@ -0,0 +1,14 @@
+#include <stddef.h>
+#include "function_pointers.h"
+
+/**
+* print_name - That prints name
+* @name: Character char
+* @f: Character void
+* Return: Always 0.
+*/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+		(f)(name);
+}
