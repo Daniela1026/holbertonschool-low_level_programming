f710deac1dd0d0a26194367e4dcb34376fbebd6d
Daniela1026 <cartagenad49@gmail.com>
2022-07-14 10:57:53 -0700

    element of an array

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,8 +2,19 @@
 #include "function_pointers.h"
 
 /**
-* print_name - That prints name
-* @name: Character char
-* @f: Character void
+* array_iterator - Element of an array
+* @array: Character int
+* @size: Character size_t
+* @action: charactr void
 * Return: Always 0.
 */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t i;
+
+	if (array != 0 && action != 0)
+	{
+		for (i = 0; i < size; i++)
+			action(array[i]);
+	}
+}
