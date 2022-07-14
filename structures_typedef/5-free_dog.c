51b59ae8a7af5e8174be8f0183259d17efd6ae1a
Daniela1026 <cartagenad49@gmail.com>
2022-07-13 18:06:22 -0700

    function that frees dosg

--- structures_typedef/5-free_dog.c
+++ structures_typedef/5-free_dog.c
@@ -9,9 +9,11 @@
 */
 void free_dog(dog_t *d)
 {
-	if (d)
+	if (d != NULL)
 	{
-		free(d->name);
+		if (d->name != NULL)
+			free(d->name);
+		if (d->owner != NULL)
 		free(d->owner);
 		free(d);
 	}
