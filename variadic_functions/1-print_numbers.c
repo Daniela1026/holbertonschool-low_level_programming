4e289d0b5bf91dffdaa201c56f7439ca17daccb6
Daniela1026 <cartagenad49@gmail.com>
2022-07-15 11:07:21 -0700

    numbers separator

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,31 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+
+/**
+* print_numbers - Numbers printed separator
+* @n: Character unsigned int
+* @separator: Character char
+*
+* Return: Always 0.
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list sep;
+	unsigned int i;
+
+	va_start(sep, n);
+	if (separator == NULL)
+		return;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(sep, unsigned int));
+		if (i != (n - 1) && separator != 0)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(sep);
+}
