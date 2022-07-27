3c5c772be1f5e89749103b08fa2986ce994045c0
Daniela1026 <cartagenad49@gmail.com>
2022-07-26 21:31:31 -0700

    actualizacion

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -8,32 +8,42 @@
 * @str: Character char pointer
 * Return: element
 */
+int _element(const char *str)
+{
+	int n;
+	for (n = 0; str[n] != '\0'; n++)
+		;
+	return (n);
+}
+
+/**                                                                                                                                                                          
+ *
+ * add_node - adds a new node at the beginning of a list_t
+ * @head: Character list_t pointer
+ * @str: Character char pointer
+ * Return: element
+ * 
+ */
+
 list_t *add_node_end(list_t **head, const char *str)
 {
 	list_t *element, *new;
-	int i = 0;
-
+	
+	new = *head;
 	element = malloc(sizeof(list_t));
-
-	if (element == NULL)
+	if (str == NULL)
 		return (NULL);
-
-	for (i = 0; str[i] != '\0'; i++)
-		
-		element->str = strdup(str);
-	element->len = i;
+	
+	element->str = strdup(str);
+	element->len = _element(str);
 	element->next = NULL;
-
 	if (*head == NULL)
-	{
 		*head = new;
-	}
 	else
 	{
-		new = *head;
-		while (new->next)
-			new = new->next;
-		new->next = element;
+		while (new->next != NULL)
+				new = new->next;
+			new->next = element;
+		}
+		return (element);
 	}
-	return (element);
-}
