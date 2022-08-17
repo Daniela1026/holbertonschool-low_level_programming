699d3b9d25967ec61cc1262575d0a958fec11c3c
Daniela1026 <cartagenad49@gmail.com>
2022-08-17 12:46:42 -0700

    update bit

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -3,25 +3,25 @@
 
 /**
 * binary_to_uint - function that converts a binary number to an unsigned int
-* @b: Character char 
+* @b: Character char
 * Return: Always 0.
 */
 unsigned int binary_to_uint(const char *b)
 {
 	int i, n, str;
 	unsigned int sum = 0, mul;
-	
+
 	if (b == NULL)
 		return (0);
-	
+
 	for (str = 0; b[str]; str++)
 	{
 		if (b[str] != 48 && b[str] != 49)
 			return (0);
 	}
-	
+
 	str--;
-	
+
 	for (i = 0; b[i]; i++)
 	{
 		mul = 1;
