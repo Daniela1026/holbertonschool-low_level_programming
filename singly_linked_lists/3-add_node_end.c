8a1d0c7ac48c61952ff1c470d4c8c2ef571c2e8d
Daniela1026 <cartagenad49@gmail.com>
2022-08-17 19:43:09 -0700

    update

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -2,20 +2,6 @@
 #include "lists.h"
 #include <stdlib.h>
 
-/**
-* add_node - adds a new node at the beginning of a list_t
-* @head: Character list_t pointer
-* @str: Character char pointer
-* Return: element
-*/
-int _element(const char *str)
-{
-	int n;
-	for (n = 0; str[n] != '\0'; n++)
-		;
-	return (n);
-}
-
 /**                                                                                                                                                                          
  *
  * add_node - adds a new node at the beginning of a list_t
@@ -27,23 +13,89 @@ int _element(const char *str)
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *element, *new;
-	
-	new = *head;
-	element = malloc(sizeof(list_t));
+	list_t *end, *temp = *head;
+
+	if (head == NULL)
+		return (NULL);
+
+	end = new_node(str);
+	if (end == NULL)
+		return (NULL);
+
+	if (*head == NULL)
+	{
+		*head = end;
+		return (*head);
+	}
+
+	while (temp->next != NULL)
+		temp = temp->next;
+
+	temp->next = end;
+
+	return (*head);
+}
+
+/**
+* _strlen - returns the lenght of a string
+*@s: poiter of character
+*Return: the length of a string
+*/
+int _strlen(const char *s)
+{
+	int len;
+
+	len = 0;
+	while (*(s + len) != '\0')
+		len++;
+	return (len);
+}
+
+/**
+* *_strdup - duplicaate a strning with new memory
+*@str: string received
+*Return: Null otherwise the duplicated string
+*/
+char *_strdup(const char *str)
+{
+	char *cp;
+	unsigned int i, j;
+
 	if (str == NULL)
 		return (NULL);
-	
-	element->str = strdup(str);
-	element->len = _element(str);
-	element->next = NULL;
-	if (*head == NULL)
-		*head = new;
-	else
+	i = _strlen(str);
+	cp = malloc(i * sizeof(char));
+
+	if (cp == NULL)
 	{
-		while (new->next != NULL)
-				new = new->next;
-			new->next = element;
-		}
-		return (element);
+		return (NULL);
 	}
+
+	for (j = 0; j < i; j++)
+	{
+		cp[j] = str[j];
+	}
+
+	return (cp);
+}
+
+/**
+* new_node - create a new node
+*@s: string to put in the node
+*Return: ponter to new node
+*/
+
+list_t *new_node(const char *s)
+{
+	list_t *add;
+
+	add = malloc(sizeof(list_t));
+
+	if (add == NULL)
+		return (NULL);
+
+	add->str = _strdup(s);
+	add->len = _strlen(s);
+	add->next = NULL;
+	return (add);
+}
