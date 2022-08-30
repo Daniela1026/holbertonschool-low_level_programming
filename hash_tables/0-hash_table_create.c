84d4a947971bc83b2358871d07cc8d763855c47a
Daniela1026 <cartagenad49@gmail.com>
2022-08-29 18:28:20 -0700

    Create a hash tables

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,8 +1,8 @@
 #include "hash_tables.h"
 
 /**
-* hash_table_create - creates a hash table.
-* @size: Character long 
+* hash_table_create - creates a hash table
+* @size: Character long
 * Return: a pointer to the newly created hash table
 */
 hash_table_t *hash_table_create(unsigned long int size)
